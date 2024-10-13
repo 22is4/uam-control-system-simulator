c492d4e8ed7c83b2d643a1db6715803ce8e3363b
dudwhdrkd <whdrod777@naver.com>
2024-10-13 22:55:32 +0900

    mavsdk를 ros2로 대체(좌표 출력)

--- mavsdk/controller/spawn_kill_drone.py
+++ mavsdk/controller/spawn_kill_drone.py
@@ -16,6 +16,8 @@ PX4_AUTOPILOT_DIR = "/home/rkdwhddud/uam-control-system-simulator/PX4-Autopilot"
 # 인스턴스 관리하는 txt 파일 경로 (환경에 따라 수정)
 INSTANCE_FILE_PATH = "/home/rkdwhddud/uam-control-system-simulator/mavsdk/controller/drone_instances.txt"
 
+ROS2_PRINT_NODE_PATH = "/home/rkdwhddud/uam-control-system-simulator/mavsdk/controller/ws_ros2_drone"
+
 def calculate_model_pose(home, target):# x, y의 좌표 계산해서 반환하는 함수
     
     # 위도 차이로 x 변위 계산, y 좌표는 동일하게 함
@@ -46,6 +48,8 @@ def spawn_drone_at_target(target_latitude, target_longitude, instance_id): # px4
         f"PX4_SYS_AUTOSTART=4001 PX4_GZ_MODEL_POSE='{y_disp},{x_disp}' "
         f"PX4_GZ_MODEL=x500 ./build/px4_sitl_default/bin/px4 -i {instance_id}"
     )
+
+    ros2_print_command = ["bash", "-c", f"source install/setup.bash && ros2 run print_position print_position {instance_id}"]
     
     # PX4-Autopilot 디렉토리로 이동
     os.chdir(PX4_AUTOPILOT_DIR)
@@ -55,12 +59,17 @@ def spawn_drone_at_target(target_latitude, target_longitude, instance_id): # px4
     # 명령어 실행 및 python PID 저장 (나중에 파이썬 프로세스가 종료될 때 자식들도 같이 종료하기 위함)
     process = subprocess.Popen(px4_command, shell=True, preexec_fn=os.setsid) # 프로세스 그룹으로 묶기
     #time.sleep(10)
-    position_process = subprocess.Popen(
-        ["python3", "/home/rkdwhddud/uam-control-system-simulator/mavsdk/controller/print_position.py", str(instance_id)],
-        preexec_fn=os.setsid,
+    # position_process = subprocess.Popen(
+    #     ["python3", "/home/rkdwhddud/uam-control-system-simulator/mavsdk/controller/print_position.py", str(instance_id)],
+    #     preexec_fn=os.setsid,
         
-    )
+    # )
     # os.setpgid(position_process.pid, os.getpgid(process.pid))
+
+    os.chdir(ROS2_PRINT_NODE_PATH)
+    # subprocess.Popen(['source', 'install/setup.bash'])
+    # print_process = subprocess.Popen(ros2_print_command, preexec_fn=os.setsid)
+    print_process = subprocess.Popen(ros2_print_command, preexec_fn=os.setsid)
 
     pid = os.getpid()  # 파이썬 프로세스 PID 추출
     # instance id와 PID 함께 저장
@@ -76,8 +85,10 @@ def spawn_drone_at_target(target_latitude, target_longitude, instance_id): # px4
         print(f"프로세스 그룹 종료: {pid}")
         os.killpg(os.getpgid(process.pid), signal.SIGINT)
         print(f"px4 - {process.pid} 종료")
-        os.killpg(os.getpgid(position_process.pid), signal.SIGINT)
-        print(f"position - {position_process.pid} 종료")
+        # os.killpg(os.getpgid(position_process.pid), signal.SIGINT)
+        # print(f"position - {position_process.pid} 종료")
+        os.killpg(os.getpgid(print_process.pid), signal.SIGINT)
+        print(f"ros2 - {print_process.pid} 종료")
         exit(0)
 
 def kill_px4_process(instance_id): # 프로세스 종료시키는 함수
